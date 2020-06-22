3d3ab9c3bded64997c760fb11c90505a2ea25c54
Daniel Norris <dan.norris@hotmail.com>
2020-06-22 16:36:24 +0100

    added routing to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
-import '../styles/main.scss';
 
+import {
+    Router,
+    Route,
+    Switch
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <h1>Hello!</h1>
-  );
+import history from '../history';
+
+const App = () => {
+    return (
+        <Router history={history}>
+            <Switch>
+
+                <Route exact path="/">
+                    <h1>Hello!</h1>
+                </Route>
+
+            <p>404. No page found.</p>
+
+            </Switch>
+
+        </Router>
+    )
 }
 
 export default App;
