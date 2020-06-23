23bd1c02f2467bdf1286b5d2ce383b6c720ec5e8
Daniel Norris <dan.norris@hotmail.com>
2020-06-23 11:43:48 +0100

    added event listeners for team inputs

--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
 class Teams extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { squad1: "", squad2: "" };
+        this.handleSquadSubmit = this.handleSquadSubmit.bind(this);
+        this.handleSquad1NameChange = this.handleSquad1NameChange.bind(this);
+        this.handleSquad2NameChange = this.handleSquad2NameChange.bind(this);
+    }
+
+    handleSquadSubmit(e) {
+        e.preventDefault();
+
+    }
+
+    handleSquad1NameChange(e) {
+        this.setState({ squad1: e.currentTarget.value });
+    }
+
+    handleSquad2NameChange(e) {
+        this.setState({ squad2: e.currentTarget.value });
     }
 
     render() {
 
         return(
             <>
-                <div className="flex">
-                    <form>
+                <form className="flex">
+
+                    <div>
                         <label htmlFor="squad1">Squad 1</label>
-                        <input type="text" id="squad1" name="squad1"></input>
+                        <input
+                            onChange={ (e) => this.handleSquad1NameChange(e) }
+                            type="text"
+                            id="squad1"
+                            name="squad1"></input>
 
                         <div>
                             <label htmlFor="kit1">
@@ -32,12 +52,16 @@ class Teams extends Component {
                             <input type="radio" id="kit3" name="kit" value="3"/>
                         </div>
 
-                        <button>Confirm</button>
+                    </div>
 
-                    </form>
-                    <form>
+                    <div>
+
                         <label htmlFor="squad2">Squad 2</label>
-                        <input type="text" id="squad2" name="squad2"></input>
+                        <input
+                            onChange={ (e) => this.handleSquad2NameChange(e) }
+                            type="text"
+                            id="squad2"
+                            name="squad2"></input>
 
                         <div>
                             <label htmlFor="kit1">
@@ -53,15 +77,13 @@ class Teams extends Component {
                             </label>
                             <input type="radio" id="kit3" name="kit" value="3"/>
                         </div>
-                        <button>Confirm</button>
-
-                    </form>
+                    </div>
 
 
-                </div>
+                </form>
 
                 <Link to="/players">
-                    <button className="btn">Confirm</button>
+                    <button onSubmit={ (e) => this.handleSquadSubmit(e) } className="btn">Confirm</button>
                 </Link>
             </>
         )
