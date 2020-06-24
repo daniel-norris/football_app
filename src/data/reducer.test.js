a688cc19f9088e36a94032189d4ddee95cde01f6
Daniel Norris <dan.norris@hotmail.com>
2020-06-24 11:25:12 +0100

    added unit testing for create teams reducer

--- src/data/reducer.test.js
+++ src/data/reducer.test.js
@@ -3,6 +3,7 @@ import reducer, {
     createTeams,
     randomiseTeams,
 } from './reducer';
+import initial from './initial';
 
 const initialState = {
     players: [],
@@ -10,17 +11,34 @@ const initialState = {
 };
 
 it('creates a player', () => {
-    // create player tests here
+    // create player creation tests here
 
-    let result = createPlayer(initialState, { player: { first: "bob" } });
+    let result = createPlayer(initialState, { player: { first: "James" } });
 
-    expect(result.players[0]).toEqual({ first: "bob" });
+    expect(result.players[0]).toEqual({ first: "James" });
+    expect(result.players).not.toBe(initialState.players);
+
+    result = createPlayer(result, { player: { first: "Jess"} })
+
+    expect(result.players[0]).toEqual({ first: "James" });
+    expect(result.players[1]).toEqual({ first: "Jess" });
+
 });
 
 it('creates two teams', () => {
-    // create player tests here
+    // create team creation tests here
 
+    let result = createTeams(initialState, { teams: { team_1: { name: "Squad Uno" } }});
+
+    expect(result.games[0]).toEqual({ team_1: { name: "Squad Uno" }});
+    expect(result.games).not.toBe(initialState.games);
+
+    result = createTeams(result, { teams: { team_1: { name: "Squad Dos" } }})
+
+    expect(result.games[0]).toEqual({ team_1: { name: "Squad Uno" }});
+    expect(result.games[1]).toEqual({ team_1: { name: "Squad Dos" }});
 });
+
 
 it('randomly assigns a team to each player', () => {
     // create player tests here
