7e5448c875f51256345d270b7aa7fa42ee37c793
Daniel Norris <dan.norris@hotmail.com>
2020-06-26 11:30:38 +0100

    removed updateplayerteam from reducer

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,13 +1,7 @@
-
-
 export const randomiseTeams = (state, { team1, team2 }) => ({
     ...state,
     team1: team1,
     team2: team2,
-})
-
-export const updatePlayerTeam = state => ({
-    ...state,
 })
 
 export const createPlayer = (state, { first, last, age, skill, position }) => ({
@@ -16,6 +10,7 @@ export const createPlayer = (state, { first, last, age, skill, position }) => ({
         ...state.players, {
             first: first,
             last: last,
+            skill: skill,
             age: age,
             position: position,
         }
@@ -40,7 +35,7 @@ export const reducer = (state, action) => {
         case "CREATE_PLAYER": return createPlayer(state, action);
         case "CREATE_TEAMS": return createTeams(state, action);
         case "UPDATE_GAME": return updateGame(state, action);
-        case "RANDOMISE_TEAMS": return updatePlayerTeam(randomiseTeams(state, action));
+        case "RANDOMISE_TEAMS": return randomiseTeams(state, action);
         default: return state;
     }
 }
